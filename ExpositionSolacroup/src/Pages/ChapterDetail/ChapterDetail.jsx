cce8afdb6ec863836d0e23a038bf6aeeae8edf68
Viktor G. <viktor.gouchet@gmail.com>
2026-05-05 11:41:47 +0200

    audio tag added and functional

--- ExpositionSolacroup/src/Pages/ChapterDetail/ChapterDetail.jsx
+++ ExpositionSolacroup/src/Pages/ChapterDetail/ChapterDetail.jsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from "react"
-import { Link } from "react-router"
-// import { changeModal } from "../ChapterItem/ChapterItem"
+import { useEffect, useRef, useState } from "react"
+import { UseApplication } from "../../Components/Provider/Provider"
 
 import "./ChapterDetail.css"
-import { UseApplication } from "../../Components/Provider/Provider"
 
 
 export const ChapterDetail = ({ setModal }) => {
@@ -16,16 +14,29 @@ export const ChapterDetail = ({ setModal }) => {
     return (
         <section id="chapter-detail">
             <div id="titles">
-                <h2>{chapters[0].title}</h2>
-                <h3>{chapters[0].subtitle}</h3>
+                <h2>{chapters[Number(currentChapter)].title}</h2>
+                <h3>{chapters[Number(currentChapter)].subtitle}</h3>
             </div>
-            <button className="btn" id="btn-play">Écouter l'histoire</button>
-            <p className="quotes">{chapters[0].quote}</p>
+
+            <div id="audio-div">
+                {/* <p>Écouter l'histoire</p> */}
+                <audio id="audio"
+                    controls
+                    controlsList="nodownload"
+                    title="Écouter l'histoire"
+                >
+                    <source src={`${chapters[currentChapter].audio}`} type="audio/mp3" />
+                </audio>
+            </div>
+
+            <p className="quotes">{chapters[Number(currentChapter)].quote}</p>
+
             <div id="columns-content">
-                <p>{chapters[0].content.col1}</p>
-                <p>{chapters[0].content.col2}</p>
-                <p>{chapters[0].content.col3}</p>
+                <p>{chapters[Number(currentChapter)].content.col1}</p>
+                <p>{chapters[Number(currentChapter)].content.col2}</p>
+                <p>{chapters[Number(currentChapter)].content.col3}</p>
             </div>
+
             <div className='div-btn-close'>
                 <button className="btn btn-close" onClick={handleClick}>Fermer</button>
             </div>
